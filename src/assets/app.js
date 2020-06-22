33afb2a8625dec0c826840beacfc8b1012da797f
ANdres espejo <jeaespejogo@unal.edu.co>
2020-06-21 20:25:53 -0500

    comparación

---
+++ src/assets/app.js
@@ -0,0 +1,18 @@
+// let valor;
+
+// valor = Symbol('simbolo');
+
+
+// //Array
+// valor = [1,2,3,4,5]
+
+
+
+// console.log(typeof valor);
+
+
+//Operadores de comparación
+
+const number1= 15;
+const number2= '15';
+
