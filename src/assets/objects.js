62c8a0bfd174cd4993025c7b9b51c9a80bc948c2
ANdres espejo <jeaespejogo@unal.edu.co>
2020-06-22 13:14:47 -0500

    Objects

---
+++ src/assets/objects.js
@@ -0,0 +1,19 @@
+// const name ='Jeff';
+
+// const numbers= [1,2,3];
+
+// numbers[0]=4;
+// numbers.push(5)
+
+// console.log(numbers);
+
+//Objetos en Javascript
+
+const person = {
+   name: 'Andi',
+   lastname: 'Espejo',
+   job: 'Engineer',
+   email: 'jefferson.espejo@gmail.com'
+}
+
+console.log(person)
