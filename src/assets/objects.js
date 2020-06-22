65b03be41ecc72cff93f49bd3631189c1ff928c9
ANdres espejo <jeaespejogo@unal.edu.co>
2020-06-22 13:43:45 -0500

    Objects

--- src/assets/objects.js
+++ src/assets/objects.js
@@ -13,7 +13,32 @@ const person = {
    name: 'Andi',
    lastname: 'Espejo',
    job: 'Engineer',
-   email: 'jefferson.espejo@gmail.com'
+   email: 'jefferson.espejo@gmail.com',
+   age: 24,
+   music: ['pop','rock','vallenato'],
+   home: {
+      city: 'Medellín',
+      country: 'Colombia'
+   },
+   fechaCumple: function() {
+      return new Date().getFullYear()-this.age;
+   }
 }
 
-console.log(person)
+console.log(person.fechaCumple())
+
+
+//Arreglo de objetos
+
+const autos = [
+   {modelo :'Mustang', motor:6.0},
+   {modelo :'Camaro', motor:8.0},
+   {modelo :'Ferrari', motor:7.0},
+]
+
+
+for(let i=0; i<autos.length; i++) {
+   console.log(autos[i])
+}
+
+
