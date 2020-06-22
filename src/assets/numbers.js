0b34630a17bc9875efdcfda82df54bd197d77c9f
ANdres espejo <jeaespejogo@unal.edu.co>
2020-06-21 20:17:22 -0500

    tipos de valores

--- src/assets/numbers.js
+++ src/assets/numbers.js
@@ -8,3 +8,50 @@ const number1=35,
 console.log(number1);
 
 let resultado;
+
+//suma 
+resultado =number1+number2;
+
+resultado =number2 - number1
+
+//modulo 
+resultado = number3 % number2
+
+//PI
+resultado= Math.PI;
+
+//redondeo
+resultado= Math.round(2.5);
+
+resultado=Math.ceil(2.1);
+
+resultado=Math.floor(2.6);
+
+//raiz cuadrada 
+resultado=Math.sqrt(144);
+
+//absolute
+resultado=Math.abs(number4);
+
+//potencia
+resultado=Math.pow(8,3);
+
+// Mínimo
+resultado= Math.min(3,5,6,7,8)
+
+//Máximo
+resultado=Math.max(5,225,19,544,78);
+
+//aleatorio
+resultado=Math.random();
+
+//descuento
+
+resultado=(10+20+20+10+5)*0.80;
+
+let puntaje =10;
+puntaje++
+//una forma de ir sumando de 1 en 1 es ++
+console.log(puntaje);
+
+console.log(resultado);
